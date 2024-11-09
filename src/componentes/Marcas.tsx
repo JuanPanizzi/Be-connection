a74a3e5cdfb2e98d85508f327e2bdaca0b78d0e0
Juan <juanpanizzi@hotmail.com>
2024-11-09 10:04:24 -0300

    Marcas component creado

---
+++ src/componentes/Marcas.tsx
@@ -0,0 +1,14 @@
+
+export const Marcas = () => {
+  return (
+    <section>
+
+<h1 className="lg:text-7xl font-thin ">MARCAS/SPONSORS</h1>
+<h2>CONOCÉ LAS MARCAS QUE NOS ACOMPAÑAN</h2>
+<p>En Be Connections elegimos cuidadosamente y damos mucho valor a las marcas y emprendedores que nos acompañan, tanto con obsequios para agasajar a nuestras invitadas, como con premios para nuestros sorteos y otras acciones.</p>
+    </section>
+
+
+
+  )
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,6 +3,7 @@ import { BeInfo } from "../componentes/home/BeInfo"
 import { Carrousel } from "../componentes/home/Carrousel"
 import { ClubBe } from "../componentes/home/ClubBe"
 import { ConocerMás } from "../componentes/home/ConocerMás"
+import { Marcas } from "../componentes/Marcas"
 
 export const Home = () => {
   return (
@@ -12,7 +13,8 @@ export const Home = () => {
       <ConocerMás/>
       <Barra text1="¿QURÉS SER PARTE DE UNO DE NUESTROS" text2="DESAYUNOS?" />
       <ClubBe/>
-      
+      <Marcas/>
+      <Barra text1="RECIBÍ INVITACIONES Y NOVEDADES EN TU WHATSAPP"/>
     </>
   )
 }
