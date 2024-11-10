0bf96de654c014a274d0a52491599d1c421ac6fc
Juan <juanpanizzi@hotmail.com>
2024-11-10 14:30:58 -0300

    advances on recomencaciones

--- src/componentes/Barra.tsx
+++ src/componentes/Barra.tsx
@@ -1,5 +1,5 @@
 
-export const Barra = ({text1, text2}:any) => {
+export const Barra = ({text1, text2, text3}:any) => {
   return (
     <>
      <div className="bg-purple1 h-72 flex justify-center items-center text-white">
@@ -7,6 +7,9 @@ export const Barra = ({text1, text2}:any) => {
         <div className="text-center flex flex-col justify-evenly items-center h-full">
 
         <h1 className="text-white lg:text-5xl w-2/3 mx-auto " style={{ lineHeight: '1.50' }}>{text1} <span className="text-rosa1"> {text2}</span></h1>
+        {
+          text3 && <p className="text-white w-2/3 text-xl py-2">{text3}</p>
+        }
         <button className="border-4 rounded border-white py-4 px-10  w-[300px] text-2xl text-white">SUMATE</button>
         </div>   
         </div>
--- src/views/Recomendaciones.tsx
+++ src/views/Recomendaciones.tsx
@@ -1,3 +1,4 @@
+import { Barra } from '../componentes/Barra'
 import { Banner } from '../componentes/Recomendaciones/Banner'
 import { RecomendacionesInfo } from '../componentes/Recomendaciones/RecomendacionesInfo'
 
@@ -6,6 +7,7 @@ export const Recomendaciones = () => {
     <>
     <Banner/>
     <RecomendacionesInfo/>
+    <Barra text1="¿QUERÉS DAR A CONOCER TU MARCA A LAS MUJERES DE NORDELTA?" text3="Hablemos sobre cómo podés incluir tu marca o dar a conocer tus productos o servicios en los eventos, producciones y desayunos de mujeres de Be Connections" />
     </>
   )
 }
