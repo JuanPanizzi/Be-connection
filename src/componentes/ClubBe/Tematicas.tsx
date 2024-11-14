9502b46dd7232fd4757a4e78d584a9d413e5a060
Juan <juanpanizzi@hotmail.com>
2024-11-14 10:30:48 -0300

    tematicas

--- src/componentes/ClubBe/Tematicas.tsx
+++ src/componentes/ClubBe/Tematicas.tsx
@@ -1,81 +1,113 @@
-
+import tematicas from '../../beConnection_Assets/club-be-imagenes/2-club-be.png'
 export const Tematicas = () => {
     return (
-        <section className="flex ">
-            <div className="lg:w-1/2 p-5">
+        <section className="flex mt-10">
+            <div className="lg:w-[60%]  lg:px-20 flex flex-col justify-end ">
+            <h1 className='lg:text-3xl pb-3 text-purple1'>ALGUNAS TEMÁTICAS DE DESAYUNOS Y EXPERIENCIAS REALIZADAS</h1>
+                <ul className=''>
+                    <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
 
-                <ul>
-                    <li className="flex p-2 border-t-2 border-rosa1">
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Rituales de mujeres: energías, chakras, conexiones.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Rituales de mujeres: energías, chakras, conexiones.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Baras de acces, tarot, círculo de mujeres.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Baras de acces, tarot, círculo de mujeres.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Yoga y masaje facial / Automasaje reflexología.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Yoga y masaje facial / Automasaje reflexología.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Arte: acuarela botánica al aire libre, talk & draw (basado en actividad de National Gallery de Londres).</span>
+                        </div>
+                        <span className="ml-2 text-xl">Arte: acuarela botánica al aire libre, talk & draw (basado en actividad de National Gallery de Londres).</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Reiki + ritual de sonido/terapias sonoras.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Reiki + ritual de sonido/terapias sonoras.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Historia del té / cata y degustación de tés.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Historia del té / cata y degustación de tés.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Cata y degustación de mate. Ceremonia del té japonés.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Cata y degustación de mate. Ceremonia del té japonés.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Degustación de vinos.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Degustación de vinos.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Workshop de gin tónic.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Workshop de gin tónic.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Música clásica con integrantes de la Orquesta Estable del Teatro Colón.</span>
+                        </div>
+                        <span className="ml-2 text-xl">Música clásica con integrantes de la Orquesta Estable del Teatro Colón.</span>
                     </li>
                     <li className="flex p-2 border-y-2 border-rosa1">
+                        <div>
+
                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                         </svg>
-                        <span className="ml-2">Make up & Skin Care junto a marcas selectivas que cumplen estándares de cuidado y calidad. </span>
+                        </div>
+                        <span className="ml-2 text-xl">Make up & Skin Care junto a marcas selectivas que cumplen estándares de cuidado y calidad. </span>
                     </li>
                
                 </ul>
             </div>
-            <div className="lg:w-1/2">
-
+            <div className="lg:w-[40%]  ">
+                <img src={tematicas} alt="" className='h-full ml-8'/>
             </div>
         </section>
     )
