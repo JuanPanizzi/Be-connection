4da434e3e391134a6dd0d0409def2f3abcfd51ef
Juan <juanpanizzi@hotmail.com>
2024-11-14 09:04:51 -0300

    news letter view

--- src/componentes/NewsLetter/NewsLetterInfo.tsx
+++ src/componentes/NewsLetter/NewsLetterInfo.tsx
@@ -6,20 +6,20 @@ export const NewsLetterInfo = () => {
   return (
     <>
 <article className='flex '>
-    <div className='w-[60%] p-10'>
+    <div className='w-[60%] py-10 px-12'>
 
           <h1 className='text-3xl md:text-6xl'>BE NEWS!</h1>
           <h2 className='text-2xl md:text-4xl md:py-6 text-purple1'>NO TE PIERDAS EL MEJOR NEWSLETTER!</h2>
-          <p className='text-lg md:text-xl montserrat'>
+          <p className='text-lg md:text-lg montserrat'>
           Con invitaciones, novedades de marcas amigas y la posibilidad de acceder a descuentos u obsequios especiales para nuestra comunidad.
           </p>
-          <p className='text-lg md:text-xl montserrat my-4'>Como ya saben, desde Be Connections organizo desayunos, encuentros, charlas y experiencias para mujeres. Casi todo esto en Nordelta, donde soy vecina y conozco muy bien. La temática de cada encuentro es una excusa, el objetivo es conectar, hacer sinergias, networking y amigas.</p>
-          <p className='text-lg md:text-xl montserrat'>Cada mes recibirás Be News!, desde donde además de contarte sobre lo que pasa en Be Connections, incluirá novedades y sorpresas como obsequios, sorteos o eventos especiales. Para esto último, necesitaría que me cuentes sobre tus intereses, así de acuerdo a tu perfil, preferencias y estilo de vida podré enviarte invitaciones a eventos especiales, producciones audiovisuales y muchas acciones divertidas que estamos planeando.</p>
-          <p  className='text-lg md:text-lg montserrat font-semibold text-purple1 mt-8'>Sumate a la Comunidad para estar al tanto de las novedades y tener prioridad para inscribirte a los encuentros, workshops y experiencias. </p>
+          <p className='text-lg md:text-lg montserrat my-4'>Como ya saben, desde Be Connections organizo desayunos, encuentros, charlas y experiencias para mujeres. Casi todo esto en Nordelta, donde soy vecina y conozco muy bien. La temática de cada encuentro es una excusa, el objetivo es conectar, hacer sinergias, networking y amigas.</p>
+          <p className='text-lg md:text-lg montserrat'>Cada mes recibirás Be News!, desde donde además de contarte sobre lo que pasa en Be Connections, incluirá novedades y sorpresas como obsequios, sorteos o eventos especiales. Para esto último, necesitaría que me cuentes sobre tus intereses, así de acuerdo a tu perfil, preferencias y estilo de vida podré enviarte invitaciones a eventos especiales, producciones audiovisuales y muchas acciones divertidas que estamos planeando.</p>
+          <p  className='text-lg md:text-base montserrat font-semibold text-purple1 mt-8'>Sumate a la Comunidad para estar al tanto de las novedades y tener prioridad para inscribirte a los encuentros, workshops y experiencias. </p>
         <Button text='SUBSCRIBIR' className='p-4 mt-10 lg:text-xl w-[250px]' />
     </div>
-    <div className='bg-red-200 w-[40%]'>
-        <img src={rose} alt="rose" className=' py-32 object-left mx-auto h-full object-contain rounded-t-[40%]'/>
+    <div className=' w-[40%]'>
+        <img src={rose} alt="rose" className='px-6 py-32 object-left mx-auto h-full object-contain rounded-t-[40%]'/>
     </div>
       
         </article>
--- src/views/Newsletter.tsx
+++ src/views/Newsletter.tsx
@@ -1,12 +1,14 @@
 import { Banner } from '../componentes/Recomendaciones/Banner';
 import mujeresNewsletter from '../beConnection_Assets/newsletter/news-letter-banner.jpg'
 import { NewsLetterInfo } from '../componentes/NewsLetter/NewsLetterInfo';
+import { Barra } from '../componentes/Barra';
 
 export const Newsletter = () => {
     return (
         <>
         <Banner imagen={mujeresNewsletter}/>
         <NewsLetterInfo/>
+        <Barra text1="RECIBÍ INVITACIONES Y NOVEDADES EN TU" text2="WHATSAPP"/>
         </>
       );
 }
