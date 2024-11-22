b2a2b23a2c04a0df3ce4ccb3753893ee754eacc5
Juan <juanpanizzi@hotmail.com>
2024-11-22 10:54:05 -0300

    avances on Carrousel

--- src/componentes/home/Carrousel.tsx
+++ src/componentes/home/Carrousel.tsx
@@ -1,45 +1,54 @@
-import mujeresDesayuno from '../../beConnection_Assets/Carrousel/13.jpg';
-
-import mujeresEspalda from '../../beConnection_Assets/Carrousel/10.jpg';
-
+import c1 from '../../beConnection_Assets/Carrousel/13.jpg';
+import c2 from '../../beConnection_Assets/Carrousel/10.jpg';
+import c3 from '../../beConnection_Assets/Carrousel/5.jpg'
+import c4 from '../../beConnection_Assets/Carrousel/4.jpg'
 import ImageGallery from 'react-image-gallery';
 import { useState } from 'react';
 
 import 'react-image-gallery/styles/css/image-gallery.css';
 import './Carrousel.css';
+import { Button } from '../shared/Button';
 
 export const Carrousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const imagenes = [
     {
-      original: mujeresDesayuno,
+      original: c1,
       title: 'DESCUBRÍ LA MAGIA DE NUESTROS DESAYUNOS Y ENCUENTROS',
-      subtitle:
-        'Tenemos más de 25 años de experiencia. Somos una pyme, y nuestra flota cuenta con chasis y semis tipo saider. Llevamos volumen y poco peso. Contamos con chasis de 5 y 10 mil kilos y semis de 15 y 22 mil kilos.'
     },
     {
-      original: mujeresEspalda,
+      original: c2,
       title: 'CONÉCTATE INSPIRATE Y CRECE JUNTO A MUJERES EXTRAORDINARIAS',
-      subtitle:
-        'Nuestro equipo está comprometido con ofrecer un servicio seguro y confiable. Nos especializamos en transporte de carga ligera y volumétrica.'
+    },
+    {
+      original: c3,
+      title: 'ÚNETE AL CLUB DE MUJERES DE NORDELTA Y POTENCIA TU CAMINO',
+    },
+    {
+      original: c4,
+      title: 'DA A CONOCER TU MARCA EN NORDELTA',
     }
   ];
 
-  const handleSlideChange = (index) => {
+  const handleSlideChange = (index: number) => {
     setCurrentIndex(index);
   };
-const desc = {desc1: "hola", desc2: "chau"}
   return (
-    <section className="relative w-full h-screen flex items-center justify-center bg-black text-white">
-      <div className="absolute z-10 text-center p-4">
+    <section className="relative w-full h-screen flex  justify-center bg-black text-white">
+      <div className="absolute z-10 text-center p-4 py-32">
         <h1
-          className="text-3xl lg:text-6xl leading-snug text-white viaoda"
-          style={{ textShadow: '1px 1px 30px black' }}
+          className="text-3xl lg:text-[4.2rem] leading-relaxed text-white viaoda "
+          style={{
+            textShadow: '2px 2px 4px black',
+            lineHeight: 1.3
+          }}
         >
           {imagenes[currentIndex].title}
         </h1>
-        <p className="mt-4 text-sm lg:text-lg">{imagenes[currentIndex].description}</p>
+
+      <button className=' max-w-[300px] text-white rounded  border-[3px] border-white p-3 w-72 sm:text-2xl mt-12   '
+      style={{ boxShadow: '2px 2px 10px black'}} >SUMATE</button>
       </div>
 
 
@@ -53,7 +62,8 @@ const desc = {desc1: "hola", desc2: "chau"}
         autoPlay={true}
         additionalClass="custom-gallery"
         onSlide={handleSlideChange}
-        description={imagenes.descripcion}
+        slideDuration={1000}
+        slideInterval={3000}
       />
     </section>
   );
