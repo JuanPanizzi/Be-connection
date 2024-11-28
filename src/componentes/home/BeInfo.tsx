69c4f79450c896fcaad4e080c5ba1e21c0d122c4
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-11-28 10:32:03 -0300

    avances on rose section

--- src/componentes/home/BeInfo.tsx
+++ src/componentes/home/BeInfo.tsx
@@ -4,37 +4,37 @@ import rose from '../../beConnection_Assets/beInfo/rose.jpg';
 export const BeInfo = () => {
     return (
         <article className="">
-            <section className="grid grid-cols-1 md:grid-cols-2 md:gap-4 md:py-6 md:gap-8">
-                <div className="  flex flex-col justify-center px-10 md:px-0 pt-10 md:pt-0 pb-2 md:pb-0 md:px-10  ">
-                    <h1 className="text-center md:text-left  text-3xl   lg:text-6xl">SOBRE BE!</h1>
-                    <h2 className="text-center md:text-left text-lg text-purple1 lg:text-[2rem] my-4  md:my-6">DE QUÉ SE TRATA BE CONNECTIONS</h2>
-                    <p className="text-sm lg:text-lg text-justify md:text-left text-gray-700 text-lg montserrat ">
+            <section className="grid grid-cols-1 sm:grid-cols-2 sm:gap-4 sm:py-6 sm:gap-8">
+                <div className="  flex flex-col justify-center px-10 sm:px-0 pt-10 sm:pt-0 pb-2 sm:pb-0 sm:px-10  ">
+                    <h1 className="text-center sm:text-left  text-3xl   lg:text-6xl">SOBRE BE!</h1>
+                    <h2 className="text-center sm:text-left text-lg text-purple1 lg:text-[2rem] my-4  sm:my-6">DE QUÉ SE TRATA BE CONNECTIONS</h2>
+                    <p className="text-sm md:text-lg text-justify sm:text-left text-gray-700  montserrat ">
                         Be Connections es una comunidad de mujeres en Nordelta que buscan generar conexiones, sinergías y amigas, de la manera más linda y divertida posible, disfrutando de desayunos, eventos y acciones junto a marcas que nos miman y nos encantan. Periódicamente realizamos desayunos temáticos: desde beauty & lifestyle a encuentros para emprendedoras y mujeres profesionales todas son oportunidades para conectar, hacer nuevas amigas o ampliar nuestra red de contactos.
                     </p>
                 </div>
-                <div className="flex justify-center  items-center p-4 md:p-8 rounded-lg">
+                <div className="flex justify-center  items-center p-4 sm:p-8 rounded-lg">
                     <img
                         src={beInfo1}
                         alt="beConnections"
-                        className=" w-full max-h-[300px] sm:max-h-[360px] md:w-[560px] md:h-[390px] object-top object-cover px-5 py-0   rounded-lg"
+                        className=" w-full max-h-[300px] sm:max-h-[360px] sm:w-[560px] sm:h-[390px] object-top object-cover px-5 py-0  rounded-lg"
                     />
                 </div>
             </section>
-            <section className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
-                <div className="flex  justify-center items-center p-4 md:p-8 rounded-lg  col-span-1">
+            <section className=" flex flex-col-reverse items-center sm:flex-row   pb-10 md:grid-cols-3 gap-4 bg-red-200 md:gap-8">
+                <div className="flex justify-center px-10 min-[500px]:h-[400px] items-center   w-full md:p-8 rounded-lg bg-lime-300 h-full bg-green-200 sm:w-[45%] ">
                     <img
                         src={rose}
                         alt="beConnections"
-                        className="w-full object-left mx-auto h-auto object-contain rounded-t-[100%]"
+                        className=" md:rounded-t-[100%] object-cover h-full  min-h-[550px]:w-full  sm:max-h-[360px] md:w-[560px] md:h-[390px]"
                     />
                 </div>
-                <div className="  flex flex-col justify-center py-16  text-right lg:p-20 col-span-2">
-                    <h1 className="text-2xl mb-2 lg:text-6xl">SOBRE ROSE</h1>
-                    <h2 className="text-xl text-purple1 lg:text-3xl my-6">¿QUIÉN ESTÁ DETRÁS DE BE CONNECTIONS?</h2>
-                    <p className="text-gray-700 text-lg montserrat">
-                    Soy Rose Alamo, Contadora y Lic. en Aminidtración con formación en Marketing y Publicidad.  Fui expatriada por 14 años donde gané experiencia y me enriquecí organizando encuentros para mujeres en diferentes ciudades, principalmente en Abu Dhabi, Emiratos Árabes. Al regresar a mi país, hace seis años, quise replicar lo mejor de lo vivido en cada ciudad (San Pablo, Abu Dhabi, Londres), creando experiencias únicas, para mujeres que aman verse bellas, valoran un estilo de vida saludable y sobre todo, desean conectar con otras mujeres, enriqueciéndose en cada encuentro unas de otras.
+                <div className=" flex flex-col justify-center px-10 bg-lime-200 pt-10 min-[550px]:pt-0 pb-2 min-[550px]:pb-0 sm:w-[55%]">
+                    <h1 className="text-center sm:text-right  text-3xl lg:text-6xl">SOBRE ROSE</h1>
+                    <h2 className="text-center sm:text-right text-lg text-purple1 lg:text-[2rem] my-4  md:my-6">¿QUIÉN ESTÁ DETRÁS DE BE CONNECTIONS?</h2>
+                    <p className="text-sm md:text-lg text-justify sm:text-right text-gray-700  montserrat">
+                    Soy Rose Alamo, Contadora y Lic. en Aministración con formación en Marketing y Publicidad.  Fui expatriada por 14 años donde gané experiencia y me enriquecí organizando encuentros para mujeres en diferentes ciudades, principalmente en Abu Dhabi, Emiratos Árabes. Al regresar a mi país, hace seis años, quise replicar lo mejor de lo vivido en cada ciudad (San Pablo, Abu Dhabi, Londres), creando experiencias únicas, para mujeres que aman verse bellas, valoran un estilo de vida saludable y sobre todo, desean conectar con otras mujeres, enriqueciéndose en cada encuentro unas de otras.
                     </p>
-                    <p className='font-semibold text-purple1 text-lg montserrat lg:mt-5'>Conocer las vivencias de cada una es muy enriquecedor!</p>
+                    <p className='text-sm md:text-lg text-justify sm:text-right font-semibold text-purple1  montserrat mt-2 lg:mt-5'>Conocer las vivencias de cada una es muy enriquecedor!</p>
                 </div>
             </section>
         </article>
