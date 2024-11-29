ed3480d2ccbccdfc17eb56d8e77687d963def97d
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-11-29 09:21:28 -0300

    barra responsive

--- src/componentes/Barra.tsx
+++ src/componentes/Barra.tsx
@@ -2,15 +2,15 @@
 export const Barra = ({text1, text2, text3}:any) => {
   return (
     <>
-     <div className="bg-purple1 h-72 flex justify-center items-center text-white">
+     <div className="bg-purple1 h-56 sm:h-72 flex justify-center items-center text-white">
         {/* <h1>{text}</h1> */}
         <div className="text-center flex flex-col justify-evenly items-center h-full">
 
-        <h1 className="text-white lg:text-5xl w-2/3 mx-auto " style={{ lineHeight: '1.50' }}>{text1} <span className="text-rosa1"> {text2}</span></h1>
+        <h1 className="text-white text-xl md:text-3xl lg:text-5xl sm:w-2/3 mx-auto " style={{ lineHeight: '1.50' }}>{text1} <span className="text-rosa1"> {text2}</span></h1>
         {
           text3 && <p className="text-white w-2/3 text-xl py-2">{text3}</p>
         }
-        <button className="border-4 rounded border-white py-4 px-10  w-[300px] text-2xl text-white">SUMATE</button>
+        <button className="border-4 rounded border-white md:py-4 md:px-10 w-[100px]  md:w-[300px] md:text-2xl text-white">SUMATE</button>
         </div>   
         </div>
     </>
