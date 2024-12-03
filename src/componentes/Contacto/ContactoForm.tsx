3103be6d7ff74881caae1b046c17877823103c55
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-12-03 14:48:29 -0300

    adv

--- src/componentes/Contacto/ContactoForm.tsx
+++ src/componentes/Contacto/ContactoForm.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export const ContactoForm = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 bg-purple1">
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-gray-800 text-center mb-2">CONTACTO</h2>
+        <h2 className="text-4xl  font-bold text-gray-800 text-center mb-2">CONTACTO</h2>
         <p className="text-gray-600 text-center mb-6">Envíanos tu consulta</p>
         
-        <form className="space-y-4">
+        <form className="space-y-4 ">
           <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">
               Nombre
