2124892677ea532e4890390482d79587df8f02ed
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-12-04 13:44:49 -0300

    avances imagen personal

--- src/componentes/Recomendaciones/ImagenPersonal/CardServicios.tsx
+++ src/componentes/Recomendaciones/ImagenPersonal/CardServicios.tsx
@@ -1,5 +1,5 @@
 
-export const CardServicios = ({ imagen, titulo, p1, p2, ig1, ig2, tel }: any) => {
+export const CardServicios = ({ imagen, titulo, p1, p2, ig1, ig2, italic, contacto }: any) => {
   return (
 
     <>
@@ -9,14 +9,16 @@ export const CardServicios = ({ imagen, titulo, p1, p2, ig1, ig2, tel }: any) =>
           alt="Imagen 1"
           className="w-full h-auto mb-4 rounded border-4 border-rosa1 mx-auto max-w-[200px] lg:max-w-[350px]"
         />
-        <div className='text-center lg:px-4'>
+        <div className='text-center lg:px-4 max-w-[300px] lg:max-w-[400px]  mx-auto'>
 
-          <h3 className="mb-2 sm:text-sm  lg:text-2xl md:text-justify lg:text-center  lora text-purple1  lg:px-1 ">{titulo}</h3>
-          <p className="text-[0.9rem] ">{p1}</p>
-          <p className="text-[0.9rem] font-semibold">{ig1}</p>
-          {ig2 && <p className="text-[0.9rem] font-semibold">{ig2}</p>}
-          <p className="text-[0.9rem] ">{p2}</p>
-          <p className="text-[0.9rem] font-semibold text-purple1">{tel}</p>
+          <h3 className="mb-2 sm:text-sm  lg:text-2xl md:text-center lg:text-center  lora text-purple1  lg:px-1 ">{titulo}</h3>
+          <p className="text-[0.9rem] sm:text-sm md:text-md lg:text-lg ">{p1}</p>
+          <p className="text-[0.9rem] sm:text-sm md:text-md lg:text-lg font-semibold">{ig1}</p>
+          {ig2 && <p className="text-[0.9rem] sm:text-sm md:text-md lg:text-lg font-semibold">{ig2}</p>}
+          <p className="text-[0.9rem] sm:text-sm md:text-md lg:text-lg ">{p2}</p>
+          {italic && <p className="text-[0.9rem] sm:text-sm md:text-md lg:text-lg"> <i> {italic} </i>Jung.</p>}
+
+          <p className="text-[0.9rem] sm:text-sm md:text-md lg:text-lg font-semibold text-purple1">{contacto}</p>
         </div>
       </div>
     </>
--- src/componentes/Recomendaciones/ImagenPersonal/Servicios.tsx
+++ src/componentes/Recomendaciones/ImagenPersonal/Servicios.tsx
@@ -4,12 +4,33 @@ import peinados from '../../../beConnection_Assets/Imagen-personal-imagenes/3.pn
 import makeUp from '../../../beConnection_Assets/Imagen-personal-imagenes/4.png'
 import asesoramiento2 from '../../../beConnection_Assets/Imagen-personal-imagenes/5.png'
 import asesoramiento3 from '../../../beConnection_Assets/Imagen-personal-imagenes/6.png'
+import coaching from '../../../beConnection_Assets/Imagen-personal-imagenes/7.png'
+import asesoramiento4 from '../../../beConnection_Assets/Imagen-personal-imagenes/8.png'
+import actitud from '../../../beConnection_Assets/Imagen-personal-imagenes/9.png'
+import asesoria from '../../../beConnection_Assets/Imagen-personal-imagenes/10.png'
+import makeUp2 from '../../../beConnection_Assets/Imagen-personal-imagenes/11.png'
+import coaching2 from '../../../beConnection_Assets/Imagen-personal-imagenes/12.png'
+
+
 import { CardServicios } from './CardServicios'
 
 export const Servicios = () => {
 
     const servicios = [
-        { imagen: asesoramiento, titulo: 'ASESORAMIENTO DE IMAGEN', p1: 'con María Pinola y Nati Pierri', ig1: '@grupoimagenmariapinola', ig2: '@nataliapierricentrodeimagen', p2: 'Asesoras de imagen. Cursos y mentorías', tel: '+5411-4426-8008 / +54911 4429-2967' }
+        { imagen: asesoramiento, titulo: 'ASESORAMIENTO DE IMAGEN', p1: 'con María Pinola y Nati Pierri', ig1: '@grupoimagenmariapinola', ig2: '@nataliapierricentrodeimagen', p2: 'Asesoras de imagen. Cursos y mentorías', contacto: '+5411-4426-8008 / +54911 4429-2967' },
+        { imagen: cabello, titulo: 'CABELLO Y BELLEZA', p1: 'Cabello y servicios manos/pies/tratamientos', ig1: '@status.nordelta', ig2: '', p2: 'Visitalos por asesoramiento de servicios de color, corte, peinados', contacto: '+5411 6164-2406' },
+        { imagen: peinados, titulo: 'PEINADOS Y MAKE UP', p1: 'Cursos/asesoramiento auto-peinado y Make up con Gigo', ig1: '@bygigo', ig2: '', p2: 'Aprendé a maquillarte y peinarte según tu estilo', contacto: '+5411 5470-3050' },
+        { imagen: makeUp, titulo: 'MAKE UP', p1: 'Auto-maquillaje y cursos de perfeccionamiento. Natalia U.', ig1: '@nataliaumakeup', ig2: '', p2: 'Asesoramiento personalizado o cursos grupales', contacto: '+5411 5954-9925' },
+        { imagen: asesoramiento2, titulo: 'ASESORAMIENTO DE IMAGEN', p1: 'Con Any Casas', ig1: '@anuchicasas', ig2: '', p2: 'Asesoramiento personalizado. Personal shopper', contacto: '+5411 5164-2506' },
+        { imagen: asesoramiento3, titulo: 'ASESORAMIENTO DE IMAGEN', p1: 'Con Flor Toro', ig1: '@fashiondebility', ig2: '', p2: 'Asesoramiento, cursos y personal shopper.', contacto: '+5411 4044-1307' },
+        { imagen: coaching, titulo: 'COACHING IMAGEN INTERIOR', p1: 'Con Magalí Sigal', ig1: '@magali_sigal', ig2: '', p2: 'Coach transgeneracional y consteladora familiar.', italic: '"Quien mira hacia afuera sueña, quien mira hacia adentro despierta"', contacto: 'magalisigal@gmail.com' },
+        { imagen: asesoramiento4, titulo: 'ASESORAMIENTO DE IMAGEN', p1: 'Con Ana Cristiani', ig1: '@anacristiani_fashion', ig2: '', p2: 'Coach de imagen integral y personal shopper.', contacto: '+5411 6420-1163' },
+        { imagen: actitud, titulo: 'TALLER DE ACTITUD', p1: 'Con Sol Villarreal', ig1: '@solvillarrealok', ig2: '', p2: 'Taller bases del modelaje para tu vida: incluye actitud - seguridad - imagen personal y redes (cómo posar).', contacto: '@solvillarrealok' },
+        { imagen: asesoria, titulo: 'ASESORIA INTEGRAL Y OUTFITS', p1: 'Con Valeria Parodi', ig1: '', ig2: '', p2: 'Ofrece cursos y mentorías además de su propia marca de ropa.', contacto: '@valeriaparodi.style / @val.parodi' },
+        { imagen: makeUp2, titulo: 'MAKE-UP', p1: 'Con Alexia', ig1: '@alexiajaet', ig2: '', p2: 'Asesoramiento y workshops.', contacto: '@alexiajaet' },
+        { imagen: coaching2, titulo: 'COACHING INDIVIDUAL', p1: 'Con Andrea Tellechea', ig1: '@andreatellecheacoach', ig2: '', p2: 'Andrea es Lic. en Administración - Coach ontológico profesional - Especialista en PNL.', contacto: 'andreatelcoach@gmail.com' },
+
+
     ]
 
     return (
@@ -17,11 +38,9 @@ export const Servicios = () => {
             {/* <div className="text-center pb-10 ">
         <h2 className="text-2xl lg:text-6xl font-thin sm:text-4xl">CONOCÉ MÁS SOBRE BE!</h2>
       </div> */}
-            <div className="grid md:grid-cols-3 md:flex-row  gap-4 px-6">
-
-
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 md:flex-row  gap-4 px-6">
                 {
-                    servicios.map(({ imagen, titulo, p1, p2, ig1, ig2, tel }, index) => (
+                    servicios.map(({ imagen, titulo, p1, p2, ig1, ig2, italic , contacto }, index) => (
 
                         <CardServicios 
                         imagen={imagen} 
@@ -30,7 +49,8 @@ export const Servicios = () => {
                         p2={p2} 
                         ig1={ig1} 
                         ig2={ig2} 
-                        tel={tel} 
+                        italic={italic}
+                        contacto={contacto} 
                         key={index} />
                     ))
                 }
