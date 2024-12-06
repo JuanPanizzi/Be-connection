e4a806b92378e1783df03bf2c20c7ccd6414a1fd
Juan <juanpanizzi@hotmail.com>
2024-12-06 14:53:55 -0300

    avances responsive sobre be info

--- src/componentes/home/BeInfo.tsx
+++ src/componentes/home/BeInfo.tsx
@@ -3,16 +3,16 @@ import rose from '../../beConnection_Assets/beInfo/rose.jpg';
 
 export const BeInfo = () => {
     return (
-        <article className="" id='be-info'>
-            <section className="xl:max-w-[1700px] mx-auto bg-green-200 grid grid-cols-1 sm:grid-cols-2 sm:gap-4 sm:py-6 sm:gap-8">
-                <div className="mx-auto bg-lime-200  flex flex-col justify-center px-10 sm:px-0 pt-10 sm:pt-0 pb-2 sm:pb-0 sm:px-10  ">
-                    <h1 className="text-center sm:text-left  text-3xl   lg:text-6xl">SOBRE BE!</h1>
-                    <h2 className="text-center sm:text-left text-lg text-purple1 lg:text-[2rem] my-4  sm:my-6">DE QUÉ SE TRATA BE CONNECTIONS</h2>
+        <article className="xl:max-w-[1700px] mx-auto" id='be-info'>
+            <section className="mx-auto  grid grid-cols-1 sm:grid-cols-2 sm:gap-4 sm:py-6 sm:gap-8">
+                <div className="mx-auto  flex flex-col justify-center px-10 sm:px-0 pt-10 sm:pt-0 pb-2 sm:pb-0 sm:px-10 xl:px-0 bg-red-200  ">
+                    <h1 className="text-center sm:text-left  text-3xl   lg:text-6xl xl:text-7xl">SOBRE BE!</h1> 
+                    <h2 className="text-center sm:text-left text-lg text-purple1 lg:text-[2rem] xl:text-[2.5rem] my-4  sm:my-6">DE QUÉ SE TRATA BE CONNECTIONS</h2>
                     <p className="text-sm md:text-lg text-justify sm:text-left text-gray-700  montserrat ">
                         Be Connections es una comunidad de mujeres en Nordelta que buscan generar conexiones, sinergías y amigas, de la manera más linda y divertida posible, disfrutando de desayunos, eventos y acciones junto a marcas que nos miman y nos encantan. Periódicamente realizamos desayunos temáticos: desde beauty & lifestyle a encuentros para emprendedoras y mujeres profesionales todas son oportunidades para conectar, hacer nuevas amigas o ampliar nuestra red de contactos.
                     </p>
                 </div>
-                <div className="flex justify-center xl:justify-end bg-red-200 items-center p-4 sm:p-6 lg:p-8 rounded-lg xl:p-0 ">
+                <div className="flex justify-center xl:justify-end  items-center p-4 sm:p-6 lg:p-8 rounded-lg xl:p-0 ">
                     <img
                         src={beInfo1}
                         alt="beConnections"
