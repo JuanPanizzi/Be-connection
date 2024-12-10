a838906c8ff2b957190048d009cf18d250a1e460
Juan <juanpanizzi@hotmail.com>
2024-12-10 10:05:27 -0300

    resized font club be

--- src/componentes/ClubBe/ClubBeInfo.tsx
+++ src/componentes/ClubBe/ClubBeInfo.tsx
@@ -5,20 +5,20 @@ import { Button } from '../shared/Button'
 export const ClubBeInfo = () => {
   return (
     <article className=" xl:max-w-[2000px] mx-auto xl:px-20 p-4  lg:p-12">
-      <section className="  grid grid-cols-1 lg:grid-cols-3 lg:gap-6  lg:gap-8">
+      <section className="  grid grid-cols-1 lg:grid-cols-3 lg:gap-6  lg:gap-8 ">
         {/* Imagen */}
-        <div className=" flex mx-auto justify-center items-center   p-4 rounded-lg col-span-1">
+        <div className=" flex  justify-center items-center   p-4 lg:p-0 rounded-lg col-span-1  mx-auto lg:mx-0  lg:mr-auto">
           <img
             src={club}
             alt="beConnections"
-            className="w-full max-w-xs lg:max-w-sm lg:max-w-lg xl:max-w-xl h-auto object-contain rounded-t-[40%]"
+            className="w-full max-w-xs lg:max-w-sm lg:max-w-lg xl:max-w-xl h-auto object-contain rounded-t-[40%] "
           />
         </div>
         
         {/* Contenido */}
         <div className="flex flex-col justify-center text-center lg:text-right py-8 lg:py-16 col-span-2">
           {/* Título y descripción */}
-          <div>
+          <div className='sm:max-w-lg sm:mx-auto lg:max-w-none lg:mx-0'>
             <h1 className="text-3xl lg:text-4xl lg:text-6xl  xl:text-7xl mb-4">CLUB BE!</h1>
             <h2 className="text-lg  lg:text-3xl xl:text-4xl text-purple1 mb-6">
               PODÉS ESTAR AL TANTO DE LAS NOVEDADES
@@ -31,7 +31,7 @@ export const ClubBeInfo = () => {
           </div>
           
           {/* Perfil y detalles */}
-          <div className="mt-8">
+          <div className="mt-8 sm:max-w-lg  sm:mx-auto lg:max-w-none lg:mx-0   ">
             <h2 className="text-lg  lg:text-3xl xl:text-4xl text-purple1 mb-6">
               ¿CUÁL ES EL PERFIL DE LAS MUJERES BE?
             </h2>
--- src/componentes/ClubBe/Tematicas.tsx
+++ src/componentes/ClubBe/Tematicas.tsx
@@ -1,9 +1,9 @@
 import tematicas from '../../beConnection_Assets/club-be-imagenes/2-club-be.png'
 export const Tematicas = () => {
     return (
-        <section className=" xl:max-w-[2000px] mx-auto lg:px-12 xl:px-20 flex flex-col-reverse lg:flex-row   mt-10 ">
-            <div className="lg:w-[60%]   flex flex-col justify-end xl:justify-start px-6 lg:px-0 ">
-                <h1 className='lg:text-3xl pb-3 text-purple1 text-center lg:text-left'>ALGUNAS TEMÁTICAS DE DESAYUNOS Y EXPERIENCIAS REALIZADAS</h1>
+        <section className=" xl:max-w-[2000px] mx-auto lg:px-12 xl:px-20 flex flex-col-reverse lg:flex-row   mt-10 sm:mb-20 lg:mb-28 ">
+            <div className="sm:max-w-lg sm:mx-auto lg:max-w-none lg:mx-0  lg:w-[60%]   flex flex-col justify-end xl:justify-start px-6 lg:px-0  ">
+                <h1 className='lg:text-3xl xl:text-4xl pb-3 text-purple1 text-center lg:text-left'>ALGUNAS TEMÁTICAS DE DESAYUNOS Y EXPERIENCIAS REALIZADAS</h1>
                 <ul className=''>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -12,7 +12,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Rituales de mujeres: energías, chakras, conexiones.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Rituales de mujeres: energías, chakras, conexiones.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -21,7 +21,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Baras de acces, tarot, círculo de mujeres.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Baras de acces, tarot, círculo de mujeres.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -30,7 +30,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Yoga y masaje facial / Automasaje reflexología.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Yoga y masaje facial / Automasaje reflexología.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -38,16 +38,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Arte: acuarela botánica al aire libre, talk & draw (basado en actividad de National Gallery de Londres).</span>
-                    </li>
-                    <li className="flex p-2 border-t-2 border-rosa1">
-                        <div>
-
-                            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
-                            </svg>
-                        </div>
-                        <span className="ml-2 text-sm lg:text-xl">Reiki + ritual de sonido/terapias sonoras.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Arte: acuarela botánica al aire libre, talk & draw (basado en actividad de National Gallery de Londres).</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -56,7 +47,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Historia del té / cata y degustación de tés.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Reiki + ritual de sonido/terapias sonoras.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -65,7 +56,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Cata y degustación de mate. Ceremonia del té japonés.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Historia del té / cata y degustación de tés.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -74,7 +65,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Degustación de vinos.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Cata y degustación de mate. Ceremonia del té japonés.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -83,7 +74,7 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Workshop de gin tónic.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Degustación de vinos.</span>
                     </li>
                     <li className="flex p-2 border-t-2 border-rosa1">
                         <div>
@@ -92,7 +83,16 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Música clásica con integrantes de la Orquesta Estable del Teatro Colón.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Workshop de gin tónic.</span>
+                    </li>
+                    <li className="flex p-2 border-t-2 border-rosa1">
+                        <div>
+
+                            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
+                            </svg>
+                        </div>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Música clásica con integrantes de la Orquesta Estable del Teatro Colón.</span>
                     </li>
                     <li className="flex p-2 border-y-2 border-rosa1">
                         <div>
@@ -101,13 +101,13 @@ export const Tematicas = () => {
                                 <rect x="-1" y="10.6318" width="16.4497" height="16.4497" rx="2.86083" transform="rotate(-45 -1 10.6318)" fill="#F0C4DD" />
                             </svg>
                         </div>
-                        <span className="ml-2 text-sm lg:text-xl">Make up & Skin Care junto a marcas selectivas que cumplen estándares de cuidado y calidad.</span>
+                        <span className="ml-2 text-base lg:text-lg xl:text-xl">Make up & Skin Care junto a marcas selectivas que cumplen estándares de cuidado y calidad.</span>
                     </li>
 
                 </ul>
             </div>
-            <div className="lg:w-[40%] mb-6 lg:mb-0  flex justify-center bg-sky-200 ">
-                <img src={tematicas} alt="" className='h-full max-[640px]:max-w-[270px] max-[768px]:max-w-[290px]   mx-auto lg:mx-0 lg:ml-auto ' />
+            <div className="lg:w-[40%] mb-6 lg:mb-0  flex justify-center  ">
+                <img src={tematicas} alt="" className='h-full max-[640px]:max-w-[270px] max-[768px]:max-w-[290px] lg:max-w-[350px] xl:max-w-[420px] lg:object-cover  mx-auto lg:mx-0 lg:ml-auto ' />
             </div>
         </section>
     )
