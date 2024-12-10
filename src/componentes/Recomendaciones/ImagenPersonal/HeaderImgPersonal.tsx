cde7c7e9cfa9c1e50a02b7fc06332c62d8fd9a69
Juan <juanpanizzi@hotmail.com>
2024-12-10 15:38:14 -0300

    Correciones ortograficas en Servicios

--- src/componentes/Recomendaciones/ImagenPersonal/HeaderImgPersonal.tsx
+++ src/componentes/Recomendaciones/ImagenPersonal/HeaderImgPersonal.tsx
@@ -6,9 +6,9 @@ export const HeaderImgPersonal = () => {
                 <h1 className="text-center sm:text-left   text-xl sm:text-2xl md:text-3xl lg:text-6xl xl:text-7xl font-thin">IMAGEN PERSONAL</h1>
                 <h2 className="text-center sm:text-left text-sm sm text-purple1 md:text-[1.2rem] lg:text-[2rem] xl:text-[2.3rem] xl:leading-snug my-4 lg:my-7">LOGRÁ FOTOS PROFESIONALES QUE HABLEN DE VOS</h2>
                 <p className="text-xs  sm:text-sm md:text-lg xl:text-xl text-justify sm:text-left text-gray-700  montserrat ">
-                    Te invito a recorrer el camino de encontrar la imagen profesional que hable de vos y bookear una sesión de fotos para lograr fotos que te representen, fotos que reflejen tu energía y pasión emprendedora. Te invito a trabajar en tu imagen, pero no solo para el día del shooting, sino para que te sirva siempre. Que en cada envento, encuentro u oportunidad de networking al que asistas, te plantes frente a la cámara con seguridad
+                    Te invito a recorrer el camino de encontrar la imagen profesional que hable de vos y bookear una sesión de fotos para lograr fotos que te representen, fotos que reflejen tu energía y pasión emprendedora. Te invito a trabajar en tu imagen, pero no solo para el día del shooting, sino para que te sirva siempre. Que en cada evento, encuentro u oportunidad de networking al que asistas, te plantes frente a la cámara con seguridad.
                 </p>
-                <p className='text-xs md:text-base xl:text-lg text-justify sm:text-left text-purple1 mt-3 font-semibold montserrat '>*Tené en cuenta que desde Be Connections solo te los presento y recomiendo. Los honorarios con cada uno de ellos, deberás consultarlos y acordarlos con ellos. Podés contactarlos sin compromiso!</p>
+                <p className='text-xs md:text-base xl:text-lg text-justify sm:text-left text-purple1 mt-3 font-semibold montserrat '>*Tené en cuenta que desde Be Connections solo te los presento y recomiendo. Los honorarios con cada uno de ellos, deberás consultarlos y acordarlos con ellos. ¡Podés contactarlos sin compromiso!</p>
             </div>
         </section>
     )
