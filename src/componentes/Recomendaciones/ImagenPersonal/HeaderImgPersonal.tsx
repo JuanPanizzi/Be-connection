a8381b856fba506dbce274171f539f685e1eb27c
Juan <juanpanizzi@hotmail.com>
2024-12-10 11:41:52 -0300

    resized font on HeaderImgPersonal

--- src/componentes/Recomendaciones/ImagenPersonal/HeaderImgPersonal.tsx
+++ src/componentes/Recomendaciones/ImagenPersonal/HeaderImgPersonal.tsx
@@ -2,13 +2,13 @@
 export const HeaderImgPersonal = () => {
     return (
         <section className="lg:px-20 sm:p-4 lg:p-0">
-            <div className="  flex flex-col justify-center px-3 sm:px-0 pt-10 sm:pt-0 pb-2 sm:pb-0 sm:px-7   lg:max-w-[900px] lg:py-10  ">
-                <h1 className="text-center sm:text-left   text-xl sm:text-2xl md:text-3xl lg:text-6xl font-thin">IMAGEN PERSONAL</h1>
-                <h2 className="text-center sm:text-left text-sm sm text-purple1 md:text-[1.2rem] lg:text-[2rem] my-4 lg:my-7">LOGRÁ FOTOS PROFESIONALES QUE HABLEN DE VOS</h2>
-                <p className="text-xs  sm:text-sm md:text-lg text-justify sm:text-left text-gray-700  montserrat ">
+            <div className="  flex flex-col justify-center px-3 sm:px-0 pt-10 sm:pt-0 pb-2 sm:pb-0 sm:px-7   lg:max-w-[900px] xl:max-w-[1000px] lg:py-10  ">
+                <h1 className="text-center sm:text-left   text-xl sm:text-2xl md:text-3xl lg:text-6xl xl:text-7xl font-thin">IMAGEN PERSONAL</h1>
+                <h2 className="text-center sm:text-left text-sm sm text-purple1 md:text-[1.2rem] lg:text-[2rem] xl:text-[2.3rem] xl:leading-snug my-4 lg:my-7">LOGRÁ FOTOS PROFESIONALES QUE HABLEN DE VOS</h2>
+                <p className="text-xs  sm:text-sm md:text-lg xl:text-xl text-justify sm:text-left text-gray-700  montserrat ">
                     Te invito a recorrer el camino de encontrar la imagen profesional que hable de vos y bookear una sesión de fotos para lograr fotos que te representen, fotos que reflejen tu energía y pasión emprendedora. Te invito a trabajar en tu imagen, pero no solo para el día del shooting, sino para que te sirva siempre. Que en cada envento, encuentro u oportunidad de networking al que asistas, te plantes frente a la cámara con seguridad
                 </p>
-                <p className='text-xs md:text-base text-justify sm:text-left text-purple1 mt-3 font-semibold montserrat '>*Tené en cuenta que desde Be Connections solo te los presento y recomiendo. Los honorarios con cada uno de ellos, deberás consultarlos y acordarlos con ellos. Podés contactarlos sin compromiso!</p>
+                <p className='text-xs md:text-base xl:text-lg text-justify sm:text-left text-purple1 mt-3 font-semibold montserrat '>*Tené en cuenta que desde Be Connections solo te los presento y recomiendo. Los honorarios con cada uno de ellos, deberás consultarlos y acordarlos con ellos. Podés contactarlos sin compromiso!</p>
             </div>
         </section>
     )
